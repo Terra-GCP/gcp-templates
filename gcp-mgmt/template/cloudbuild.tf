ed6d6259871926b580dfe4c294934315e3b33ca7
ayushshukla <ayush25shukla@gmail.com>
2023-03-27 18:21:45 +0000

    Push-27-March-2023-11:52PM

--- gcp-mgmt/template/cloudbuild.tf
+++ gcp-mgmt/template/cloudbuild.tf
@@ -1,6 +1,25 @@
+#.................................... Cloud Build Connection .................................#
+
+module "build_connection" {
+  for_each = {
+    for k, v in var.build_connection: k => v
+    }
+  source                        = "git@github.com:Terra-GCP/gcp-kitchen-modules//terraform-google-cloud-build-connection"
+  location                      = each.value.location
+  connection_name               = each.value.connection_name
+  app_id                        = each.value.app_id
+  oauth_token                   = each.value.oauth_token
+  project_id                    = each.value.project_id
+  repo_name                     = each.value.repo_name 
+  remote_uri                    = each.value.remote_uri   
+}
+
 #...................................... Cloud Build Trigger ...................................#
 
-/* module "build-trigger" {
+/* module "build_trigger" {
+  for_each = {
+    for k, v in var.build_trigger: k => v
+    }
   source                        = "git@github.com:Terra-GCP/gcp-kitchen-modules//terraform-google-cloud-build-trigger"
   trigger_name                  = "gcp-tfe-destruction"
   location                      = "europe-north1"
